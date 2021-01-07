b79d187bd711153ae8a569c3f8523caa8acc6089
Mahdi Pakdaman <mahipakdaman@gmail.com>
2021-01-08 00:02:24 +0330

    Product model created

--- website/models.py
+++ website/models.py
@@ -8,3 +8,7 @@ class Customer(models.Model):
     text=models.CharField(max_length=100,blank=False)
     image=models.ImageField(upload_to=None,height_field=500,width_field=500)
     sample=models.URLField(max_length=128,blank=True,db_index=False)
+
+class Product(models.Model):
+    text=models.CharField(max_length=100)
+    image=models.ImageField(upload_to=None,width_field=500,height_field=500)
