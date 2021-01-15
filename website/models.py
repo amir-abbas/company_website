91a3aa3c8b18b54223aca4728f5864b47ea2de00
Mahdi Pakdaman <mahipakdaman@gmail.com>
2021-01-16 00:22:27 +0330

    Menu model created

--- website/models.py
+++ website/models.py
@@ -8,7 +8,7 @@ class AboutUs(models.Model):
 class Customer(models.Model):
     text=models.CharField(max_length=100,blank=False)
     image=models.ImageField(upload_to=None,height_field=500,width_field=500)
-    sample=models.URLField(max_length=128,blank=True,db_index=False)
+    sample=models.URLField(max_length=256,blank=True,db_index=True)
 
 class Product(models.Model):
     text=models.CharField(max_length=100)
@@ -18,3 +18,10 @@ class ContactWithUs(models.Model):
     text=models.CharField(max_length=1000)
     image=models.ImageField(upload_to=None,width_field=500,height_field=500,blank=False)
     email=MultiEmailField()
+    
+class Menu(models.Model):
+    title=models.CharField(max_length=100,blank=False)
+    subtitle=models.CharField(max_length=100,blank=True)
+    URL=models.URLField(max_length=256,blank=True,db_index=True)
+    is_hidden=models.BooleanField(default=False)
+    sort=models.PositiveIntegerField(default=1)
